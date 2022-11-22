5a5de0e54889cc132fdea3256c9a125f69b46e6a
Liika Gumerova <gmrvl@mail.ru>
2022-11-22 07:31:28 +0300

    nn

---
+++ train.py
@@ -0,0 +1,104 @@
+import segmentation_models as sm
+from matplotlib import pyplot as plt
+from tensorflow import keras
+# from classification_models import keras
+
+from argumentations import get_training_augmentation, get_preprocessing, get_validation_augmentation
+from main import Dataset, x_train_dir, y_train_dir, x_valid_dir, y_valid_dir, Dataloder
+
+# sm.set_framework('keras')
+
+BACKBONE = 'efficientnetb3'
+BATCH_SIZE = 8
+CLASSES = ['car']
+LR = 0.0001
+EPOCHS = 40
+
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+# define network parameters
+n_classes = 1 if len(CLASSES) == 1 else (len(CLASSES) + 1)  # case for binary and multiclass segmentation
+activation = 'sigmoid' if n_classes == 1 else 'softmax'
+
+#create model
+model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
+
+# define optomizer
+optim = keras.optimizers.Adam(LR)
+
+# Segmentation models losses can be combined together by '+' and scaled by integer or float factor
+dice_loss = sm.losses.DiceLoss()
+focal_loss = sm.losses.BinaryFocalLoss() if n_classes == 1 else sm.losses.CategoricalFocalLoss()
+total_loss = dice_loss + (1 * focal_loss)
+
+# actulally total_loss can be imported directly from library, above example just show you how to manipulate with losses
+# total_loss = sm.losses.binary_focal_dice_loss # or sm.losses.categorical_focal_dice_loss
+
+metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+
+# compile keras model with defined optimozer, loss and metrics
+model.compile(optim, total_loss, metrics)
+
+# Dataset for train images
+train_dataset = Dataset(
+    x_train_dir,
+    y_train_dir,
+    classes=CLASSES,
+    augmentation=get_training_augmentation(),
+    preprocessing= get_preprocessing(preprocess_input),
+)
+
+# Dataset for validation images
+valid_dataset = Dataset(
+    x_valid_dir,
+    y_valid_dir,
+    classes=CLASSES,
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocess_input),
+)
+
+train_dataloader = Dataloder(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+valid_dataloader = Dataloder(valid_dataset, batch_size=1, shuffle=False)
+
+# check shapes for errors
+assert train_dataloader[0][0].shape == (BATCH_SIZE, 320, 320, 3)
+assert train_dataloader[0][1].shape == (BATCH_SIZE, 320, 320, n_classes)
+
+# define callbacks for learning rate scheduling and best checkpoints saving
+callbacks = [
+    keras.callbacks.ModelCheckpoint('./best_model.h5', save_weights_only=True, save_best_only=True, mode='min'),
+    keras.callbacks.ReduceLROnPlateau(),
+]
+
+
+# train model
+
+history = model.fit_generator(
+    train_dataloader,
+    steps_per_epoch=len(train_dataloader),
+    epochs=EPOCHS,
+    callbacks=callbacks,
+    validation_data=valid_dataloader,
+    validation_steps=len(valid_dataloader),
+)
+
+# Plot training & validation iou_score values
+plt.figure(figsize=(30, 5))
+plt.subplot(121)
+plt.plot(history.history['iou_score'])
+plt.plot(history.history['val_iou_score'])
+plt.title('Model iou_score')
+plt.ylabel('iou_score')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+
+# Plot training & validation loss values
+plt.subplot(122)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
